e63ada20e06dd8a6ae5c5d74074a733db1b13455
lporebski <leszek.porebski@smtsoftware.com>
2015-08-31 16:33:35 +0200

    Reformat code

--- XPathTester/Form1.cs
+++ XPathTester/Form1.cs
@@ -1,114 +1,127 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
-using HtmlAgilityPack;
 using System.Diagnostics;
+using System.Windows.Forms;
 
-namespace XPathTester {
-  public partial class Form1 : Form {
-    IXPathTool xpathTool;
+namespace XPathTester
+{
+    public partial class Form1 : Form
+    {
+        private IXPathTool xpathTool;
 
-    public Form1() {
-      InitializeComponent();
+        public Form1()
+        {
+            InitializeComponent();
 
-      this.xpathQuery.Clear();
-      this.xpathResults.Items.Clear();
-      this.xmlView.Text = string.Empty;
-      this.xpathError.Text = string.Empty;
+            xpathQuery.Clear();
+            xpathResults.Items.Clear();
+            xmlView.Text = string.Empty;
+            xpathError.Text = string.Empty;
 
-      RegisterDebug();
-    }
-
-    private void openFile_Click(object sender, EventArgs e) {
-      DialogResult result = this.openXML.ShowDialog();
-
-      if (result == System.Windows.Forms.DialogResult.OK) {
-        createTool();
-        showXml();
-      }
-    }
-
-    private void showXml() {
-      this.xmlView.TextChanged -= xmlView_TextChanged;
-      this.xmlView.Clear();
-
-      if (this.xpathTool != null) {
-        this.xmlView.Text = this.xpathTool.GetDocumentString();
-        this.xmlView.TextChanged += xmlView_TextChanged;
-      }
-    }
-
-    private void createTool() {
-      this.xpathTool = ToolsFactory.Create(this.openXML.FileName, this.xmlView.Text);
-    }
-
-    private void xmlView_TextChanged(object sender, EventArgs e) {
-      if (this.xpathTool == null) {
-        createTool();
-      }
-
-      if (this.xpathTool != null) {
-        this.xpathTool.ApplyChanges(this.xmlView.Text);
-        RefreshResultsView();
-      }
-    }
-
-    private void xpathQuery_TextChanged(object sender, EventArgs e) {
-      if (this.xpathTool == null) {
-        createTool();
-      }
-
-      if (this.xpathTool != null) {
-        this.xpathTool.Query(this.xpathQuery.Text);
-        RefreshResultsView();
-      }
-    }
-
-    private void RefreshResultsView() {
-      this.xpathResults.Items.Clear();
-      this.xpathError.Text = this.xpathTool.Error;
-      if (this.xpathTool.QueryResults == null)
-        return;
-
-      this.xpathResults.SuspendLayout();
-      {// klamra tylko po to, by zaznaczyć suspendlayout i resumelayout
-        foreach (XPathResult r in this.xpathTool.QueryResults) {
-          this.xpathResults.Items.Add(new ListViewItem(r.Tuple));
+            RegisterDebug();
         }
-      }
-      this.xpathResults.ResumeLayout();
+
+        private void openFile_Click(object sender, EventArgs e)
+        {
+            var result = openXML.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                createTool();
+                showXml();
+            }
+        }
+
+        private void showXml()
+        {
+            xmlView.TextChanged -= xmlView_TextChanged;
+            xmlView.Clear();
+
+            if (xpathTool != null)
+            {
+                xmlView.Text = xpathTool.GetDocumentString();
+                xmlView.TextChanged += xmlView_TextChanged;
+            }
+        }
+
+        private void createTool()
+        {
+            xpathTool = ToolsFactory.Create(openXML.FileName, xmlView.Text);
+        }
+
+        private void xmlView_TextChanged(object sender, EventArgs e)
+        {
+            if (xpathTool == null)
+            {
+                createTool();
+            }
+
+            if (xpathTool != null)
+            {
+                xpathTool.ApplyChanges(xmlView.Text);
+                RefreshResultsView();
+            }
+        }
+
+        private void xpathQuery_TextChanged(object sender, EventArgs e)
+        {
+            if (xpathTool == null)
+            {
+                createTool();
+            }
+
+            if (xpathTool != null)
+            {
+                xpathTool.Query(xpathQuery.Text);
+                RefreshResultsView();
+            }
+        }
+
+        private void RefreshResultsView()
+        {
+            xpathResults.Items.Clear();
+            xpathError.Text = xpathTool.Error;
+            if (xpathTool.QueryResults == null)
+                return;
+
+            xpathResults.SuspendLayout();
+            {
+// klamra tylko po to, by zaznaczyć suspendlayout i resumelayout
+                foreach (var r in xpathTool.QueryResults)
+                {
+                    xpathResults.Items.Add(new ListViewItem(r.Tuple));
+                }
+            }
+            xpathResults.ResumeLayout();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (xpathTool == null)
+            {
+                createTool();
+            }
+
+            RefreshResultsView();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            xpathTool = ToolsFactory.Create(@"e:\prog\Stock\XPathTester_PROJECT\XPathTester\sample.html", string.Empty);
+            showXml();
+            xpathQuery.Text = @"//*[@name='_searchCategory']";
+        }
+
+        [Conditional("DEBUG")]
+        private void RegisterDebug()
+        {
+            var b = new Button();
+            b.AutoSize = true;
+            b.Location = openFile.Location;
+            b.Text = "Debug refresh";
+            b.Click += button1_Click;
+            Controls.Add(b);
+
+            Load += Form1_Load;
+        }
     }
-
-
-    private void button1_Click(object sender, EventArgs e) {
-      if (this.xpathTool == null) {
-        createTool();
-      }
-
-      RefreshResultsView();
-    }
-
-    private void Form1_Load(object sender, EventArgs e) {
-      this.xpathTool = ToolsFactory.Create(@"e:\prog\Stock\XPathTester_PROJECT\XPathTester\sample.html", string.Empty);
-      showXml();
-      this.xpathQuery.Text = @"//*[@name='_searchCategory']";
-    }
-
-    [Conditional("DEBUG")]
-    private void RegisterDebug() {
-      Button b = new Button();
-      b.AutoSize = true;
-      b.Location = this.openFile.Location;
-      b.Text = "Debug refresh";
-      b.Click += button1_Click;
-      this.Controls.Add(b);
-
-      this.Load += Form1_Load;
-    }
-  }
 }
--- XPathTester/Program.cs
+++ XPathTester/Program.cs
@@ -1,18 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Windows.Forms;
 
-namespace XPathTester {
-  static class Program {
-    /// <summary>
-    /// The main entry point for the application.
-    /// </summary>
-    [STAThread]
-    static void Main() {
-      Application.EnableVisualStyles();
-      Application.SetCompatibleTextRenderingDefault(false);
-      Application.Run(new Form1());
+namespace XPathTester
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///     The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
     }
-  }
 }
